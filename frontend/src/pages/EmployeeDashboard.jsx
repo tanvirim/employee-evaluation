30c34149f2b1c320db49a71427cb2ef5ab7ae79a
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-10-01 07:11:23 +0600

    functionality add

--- frontend/src/pages/EmployeeDashboard.jsx
+++ frontend/src/pages/EmployeeDashboard.jsx
@@ -1,15 +1,9 @@
 
 import Logout from "../components/Logout"
-import ProgressList from "../components/progress/ProgressList"
-
-
-
 
 const EmployeeDashboard = () => {
   return (
     <>
-    
-   <ProgressList/>
 
     <Logout> <a >LogOut</a></Logout>
     </>
